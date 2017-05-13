92bb7d59c13983e4f24e570c18e1b85cda3035cc
Jeremy Hanshaw <jhanshaw@jeremys-air.hsd1.il.comcast.net>
2017-05-13 14:52:11 -0500

    Initial Commit

---
+++ RGBsliders/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RGBsliders
+//
+//  Created by Jeremy Hanshaw on 5/13/17.
+//  Copyright © 2017 Jeremy Hanshaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
