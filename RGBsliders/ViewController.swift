9896f19f5dff022088b96e72581fb3f0d81c158c
Jeremy Hanshaw <jhanshaw@jeremys-air.hsd1.il.comcast.net>
2017-05-13 21:02:00 -0500

    end of 3.2.3

--- RGBsliders/ViewController.swift
+++ RGBsliders/ViewController.swift
@@ -9,10 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var colorSquare: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        updateBackgroundColor(redSlider)
+        colorSquare.layer.borderColor = UIColor.black.cgColor
+        colorSquare.layer.borderWidth = 1
+    }
+    @IBAction func updateBackgroundColor(_ sender: Any) {
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
     }
 
     override func didReceiveMemoryWarning() {
